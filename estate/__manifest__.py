67c9a9cab9bffc4e3de1dd02c540855fe3368cfb
Adsandro Carvalho <adsandro@debian-BULLSEYE-live-builder-AMD64>
2022-11-28 12:07:43 -0300

    Configuração inicial do modulo

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Adsandro Carvalho",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': ['security/ir.model.access.csv'],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    
+    'application': True,
+}
